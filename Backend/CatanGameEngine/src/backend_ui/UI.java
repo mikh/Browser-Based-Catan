e1a37b71b5d112a6d5bb0c83e7110e7fb07395cb
Mikhail Andreev <mikha.andreev@gmail.com>
2014-09-06 19:36:17 -0400

    Began work on back-end UI

--- Backend/CatanGameEngine/src/backend_ui/UI.java
+++ Backend/CatanGameEngine/src/backend_ui/UI.java
@@ -1,5 +1,19 @@
 package backend_ui;
 
-public class UI {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class UI {
+	JFrame main_frame;
+	public UI(){
+		main_frame = new JFrame("Browser Based Catan - Backend");
+		main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		main_frame.setSize(800, 800);
+		
+		main_frame.setVisible(true);
+	}
+}
+
+class gui_tile extends JPanel{
+	
 }
--- Backend/CatanGameEngine/src/control_structure/Control.java
+++ Backend/CatanGameEngine/src/control_structure/Control.java
@@ -1,5 +1,15 @@
 package control_structure;
 
-public class Control {
+import backend_ui.UI;
 
+public class Control {
+	public static void main(String[] args){
+		System.out.println("Starting Test...");
+		long start_time = System.currentTimeMillis();
+		
+		UI gui = new UI();
+		
+		System.out.println("Test Complete.");
+		System.out.println(String.format("Test took %d ms.", System.currentTimeMillis() - start_time));
+	}
 }
